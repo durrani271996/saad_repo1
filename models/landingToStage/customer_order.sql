608d41be233820a8f4815e68684f5d072ff4134b
Saad Imam Duraani <saadimamduraani@gmail.com>
2024-09-10 15:05:43 +0000

    ok

---
+++ models/landingToStage/customer_order.sql
@@ -0,0 +1,27 @@
+with customer_data as (
+
+select customerkey,customername,customeraddress,customerphone from
+{{ref("customers")}}
+
+),
+
+
+
+order_data as (
+
+    select customerkey,count(total_orders) as total_orders,sum(total_revenue) as total_revenue
+    from 
+    {{ref("orders")}}
+group by customerkey
+)
+
+select cd.customerkey,
+cd.customername,
+cd.customeraddress,
+cd.customerphone,
+od.total_orders,
+od.total_revenue
+from
+customer_data cd
+left join order_data od on
+cd.customerkey=od.customerkey
